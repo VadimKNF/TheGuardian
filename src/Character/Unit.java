e287d6ecc7451a7f9d698955fe885bd594bc0bfa
Batya <safgg7@mail.ru>
2019-10-23 23:49:48 +0400

    DONT TOUCH THIS CODE ANYMORE

--- src/Character/Unit.java
+++ src/Character/Unit.java
@@ -1,33 +1,26 @@
 package Character;
 
-class Unit {
+class Unit extends RoleClass {
     private String name;
-    private int strength;
-    private int intellect;
     private int weaponStat;
     private int armorStat;
     private int healthPoint;
 
     public void setName(String name){ this.name = name; }
-    public void setStrength(int strength){ this.strength = strength; }
-    public void setIntellect(int intellect){ this.intellect = intellect; }
     public void setWeaponStat(int weaponStat){ this.weaponStat = weaponStat; }
     public void setArmorStat(int armorStat){ this.armorStat = armorStat; }
     public void setHealthPoint(int healthPoint){ this.healthPoint = healthPoint; }
 
     public String getName(){ return name; }
-    public int getStrength(){ return strength; }
-    public int getIntellect(){ return intellect; }
     public int getWeaponStat(){ return weaponStat; }
     public int getArmorStat(){ return armorStat; }
     public int getHealthPoint(){ return healthPoint; }
 
     Unit(){}
 
-    Unit (String name, int strength, int intellect, int weaponStat, int armorStat, int healthPoint) {
+    Unit (String name, String className, int strength, int intellect, int leadership, int weaponStat, int armorStat, int healthPoint) {
+        super (className, strength, intellect, leadership);
         this.name = name;
-        this.strength = strength;
-        this.intellect = intellect;
         this.weaponStat = weaponStat;
         this.armorStat = armorStat;
         this.healthPoint = healthPoint;
@@ -36,6 +29,6 @@ class Unit {
 
 class UnitsList{
     public static void main(String[] args) {
-        Unit wolf = new Unit("wolf", 3, 1, 3, 1, 100);
+        Unit wolf = new Unit("wolf", "enemy", 3, 1, 0, 3, 1, 100);
     }
 }
